38f937d1e387133a2a870f2a67e962819c1aa4ee
hmunozb <humberto.munozb@gmail.com>
2021-05-18 19:05:41 -0700

    implement ramped beta schedule

--- bin/cell_anneal.py
+++ bin/cell_anneal.py
@@ -2,23 +2,31 @@ import argparse
 import numpy as np
 import pegasustools as pgt
 from pegasustools.util.adj import read_ising_adjacency
+from pegasustools.util.sched import interpret_schedule
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--verbose", action='store_true')
 parser.add_argument("--targets", nargs='+', type=int, help="The list of target logical states to keep track of.")
-parser.add_argument("--tf", type=float, help="The anneal time for a simple annealing schedule.")
+parser.add_argument("--tf", type=float, help="The anneal time for a simple annealing schedule.", default=20.0)
 parser.add_argument("--schedule", nargs='+', help="Anneal schedule specification ")
-parser.add_argument("problem", required=True,
+parser.add_argument("--rev-init")
+parser.add_argument("problem",
                     help="A cell problem, specified in a text file with three columns with the adjacency list")
-parser.add_argument("output", required=True, help="Output file to print summary of samples.")
+parser.add_argument("output", help="Output file to print summary of samples.")
 
 args = parser.parse_args()
 
 # Import the problem and prepare the BQM
 problem_file = args.problem
 bqm = read_ising_adjacency(problem_file)
+print(bqm)
 
-
+# Interpret and construct the annealing schedule
+tf = args.tf
+if args.schedule is not None:
+    sched = interpret_schedule(args.tf, *args.schedule)
+else:
+    sched = None
 
 
 
--- pegasustools/util/sched.py
+++ pegasustools/util/sched.py
@@ -3,10 +3,10 @@ import scipy
 from scipy.special import betainc, betaincinv
 
 
-def beta_schedule(sc, lin_pnts, log_pnts, a, b):
+def beta_schedule(a, b, sc, lin_pnts, log_pnts):
     lin_space = sc / (lin_pnts + 1)
 
-    lin_s = [lin_space * i for i in range(lin_pnts + 1)]
+    lin_s = [lin_space * i for i in range(lin_pnts + 2)]
 
     log_s = []
     d = (1.0 - sc) / 2.0
@@ -19,3 +19,41 @@ def beta_schedule(sc, lin_pnts, log_pnts, a, b):
     sched_pnts = [(betaincinv(a, b, s), s) for s in s_pnts]
 
     return sched_pnts
+
+
+def ramped_beta_schedule(a, b, sq, tf, tr, sc=0.9, lin_pnts=3, log_pnts=5):
+    sched0 = beta_schedule(a, b, sc, lin_pnts, log_pnts)
+    sched1 = [(tf*t, sq*s) for (t, s) in sched0]
+    sched1.append((tf + tr, 1.0))
+    return sched1
+
+
+def args_to_sched(*sched_args):
+    nargs = len(sched_args)
+    if nargs % 2 != 0:
+        raise RuntimeError("Expected even number of points for schedule.")
+
+
+available_schedules = {
+    'pl': ("Piecewise Linear", "s0 t0 s1 t1 ..."),
+    'beta': ("Ramped beta schedule (Boundary cancelation protocol)", "a b sq [sc]")
+}
+
+
+def interpret_schedule(tf, *sched_tokens):
+    sched_name = sched_tokens[0]
+    sched_args = sched_tokens[1:]
+    nargs = len(sched_args)
+    try:
+        if sched_name == "pl":
+            pnts = args_to_sched(*sched_args)
+        elif sched_name == "beta":
+            if nargs != 3:
+                raise RuntimeError("Expected at least three arguments: a b sq [sc]")
+            a, b, sq = int(sched_args[0]), int(sched_args[1]), float(sched_args[2])
+            return ramped_beta_schedule(a, b, sq, tf, 1.0)
+        else:
+            raise RuntimeError(f"Schedule type {sched_name} not recognized")
+
+    except Exception as e:
+        raise RuntimeError(f"An exception occurred interepreting schedule type {sched_name}")
