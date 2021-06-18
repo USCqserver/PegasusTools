91d42c563234695e9f21596b0d7b366d093e0ce1
hmunozb <humberto.munozb@gmail.com>
2021-06-17 21:46:41 -0700

    Added titrate.py

---
+++ pegasustools/titrate.py
@@ -0,0 +1,119 @@
+import dimod
+import numpy as np
+from dimod.core.bqm import BQM
+from dimod.bqm import AdjVectorBQM
+from pegasustools.util import bootstrap_apply, bayesian_bootstrap, apply_boots
+
+class QuboTitration:
+    """
+    This class uses a given sampler to ``titrate'' a BQM in QUBO form
+
+      H(n) = \sum_i (a_i + h) n_i + \sum_{ij} W_{ij} n_i n_j
+
+    where h is a parameter that is varied
+
+    """
+    def __init__(self, quadratic: np.ndarray):
+        di = np.diag_indices_from(quadratic)
+        n = quadratic.shape[0]
+        self._linear = quadratic[di]
+        self._quadratic = quadratic*(1.0 - np.eye(n))
+        self.max_j = np.max(self._quadratic)
+        self._scaled_quadratic = self._quadratic/self.max_j
+        self._qubo_bqm: BQM = AdjVectorBQM(self._linear, self._quadratic, dimod.BINARY)
+
+
+    def titr_bqm(self, h):
+        linear = self._linear + h
+        bqm: BQM = AdjVectorBQM(linear, self._quadratic, dimod.BINARY)
+        return bqm
+
+    def sample_h(self, h, sampler: dimod.Sampler, titr_scale=False, titr_beta=None, **kwargs):
+        linear = self._linear + h
+        if titr_scale:
+            bqm: BQM = AdjVectorBQM(linear/self.max_j, self._scaled_quadratic, dimod.BINARY)
+        else:
+            bqm : BQM= AdjVectorBQM(linear, self._quadratic, dimod.BINARY)
+
+        results: dimod.SampleSet = sampler.sample(bqm, **kwargs)
+        results = results.aggregate()
+        titration = TitrationResult(bqm, results, beta=titr_beta)
+        return titration
+
+
+class TitrationResult:
+    def __init__(self, bqm: BQM, results: dimod.SampleSet, beta=None):
+        self.qubo_bqm = bqm
+        self.qubo_results = results
+        ising_bqm : BQM = bqm.change_vartype(dimod.SPIN, inplace=False)
+        results = results.change_vartype(dimod.SPIN, inplace=False)
+        self.ising_bqm = ising_bqm
+        self.ising_results = results
+
+        samp: np.ndarray = results.record.sample
+        energies: np.ndarray = results.record.energy
+        min_energy = np.min(energies)
+        n: np.ndarray = results.record.num_occurrences
+        total_n = np.sum(n)
+        logn = np.log(n) - np.log(total_n)
+        if beta is not None: # Set importance sampling weights
+            f_arr = beta*(energies - min_energy) + logn
+            minf = np.min(f_arr)
+            f_arr -= minf
+            w_arr = np.exp(-f_arr)
+            z = np.sum(w_arr)
+            weights = w_arr / z
+        else:
+            weights = None
+        self.sample_weights = weights
+        # First moments
+        #mean_s = np.sum(samp * weights[:, np.newaxis], axis=0)
+        #mean_energy = bootstrap_apply(np.mean, energies, weights=weights)
+        #mean_energy = bayesian_bootstrap(energies, observations=n)
+        sisj = samp[:, :, np.newaxis] * samp[:, np.newaxis, :]
+        mean_s, mean_s2, mean_energy = bayesian_bootstrap((samp, sisj, energies), observations=n, weights=weights)
+        #mean_s = bootstrap_apply(lambda x: np.mean(x, axis=0), samp, weights=weights)
+        # Calculate the mean-field energy
+        def mean_field_energy(x):
+            ms, labels = dimod.as_samples(x)
+            ldata, (irow, icol, qdata), offset \
+                = self.qubo_bqm.to_numpy_vectors(variable_order=labels)
+            qubo_x = (ms+1.0)/2.0
+            mf_energies = qubo_x.dot(ldata) + (qubo_x[:, irow] * qubo_x[:, icol]).dot(qdata) + offset
+            mf_energy = mf_energies[0]
+            return mf_energy
+        mf_energy = mean_s.apply(mean_field_energy)
+
+        def cov_boots(si_boots, sisj_boots):
+            return sisj_boots - si_boots[:, np.newaxis, :]*si_boots[:, :, np.newaxis]
+
+        def eval_cov(x):
+            samp = x[0]
+            s_is_j = x[1]
+            s_i = np.mean(samp, axis=0)
+            #s_is_j = x[:, :, np.newaxis] * x[:, np.newaxis, :]
+            return np.mean(s_is_j, axis=0) - s_i[np.newaxis, :]*s_i[:, np.newaxis]
+
+        #mean_s2 = np.sum(sisj * weights[:, np.newaxis, np.newaxis], axis=0)
+        #mean_s2 = bootstrap_apply(lambda x: np.mean(x, axis=0), sisj, weights=weights )
+        #mean_s2 = bayesian_bootstrap(sisj, observations=n)
+        # Covariance matrix
+        cov = apply_boots(cov_boots, mean_s, mean_s2)
+        #cov = mean_s2.mean - mean_s.mean[np.newaxis, :] * mean_s.mean[:, np.newaxis]
+        #cov = bootstrap_apply(eval_cov, [samp, sisj], weights=weights)
+        # Mean field energy
+        #ms = dimod.as_samples(mean_s)
+        # Use the slow way of calculating energy
+
+        # Correlation energy
+        dg_cor = apply_boots(lambda x,y: x-y, mf_energy, mean_energy)
+
+        self.energies = mean_energy
+        self.mf_energy = mf_energy
+        self.mean_s = mean_s
+        self.mean_s2 = mean_s2
+        self.cov = cov
+        self.dg_cor = dg_cor
+
+
+
