6d8ae9ace34534b7a7842dd8ff6ef1a813df8ec7
hmunozb <humberto.munozb@gmail.com>
2021-07-20 15:37:14 -0700

    add setup.py and change bins to entry points

---
+++ pegasustools/apps/cell_anneal.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+import argparse
+
+from pegasustools.app import add_general_arguments,add_cell_arguments, save_cell_results, run_sampler
+from pegasustools.util.adj import read_ising_adjacency
+from pegasustools.util.sched import interpret_schedule
+from pegasustools.pqubit import PegasusCellEmbedding
+from dwave.system import DWaveSampler
+import dimod
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    add_general_arguments(parser)
+    add_cell_arguments(parser)
+
+    args = parser.parse_args()
+
+    # Import the problem and prepare the BQM
+    problem_file = args.problem
+    bqm = read_ising_adjacency(problem_file, args.scale_j)
+    print(bqm)
+
+    # Connect to the DW sampler
+    dw_sampler = DWaveSampler()
+
+    # Interpret and construct the annealing schedule
+    tf = args.tf
+    if args.schedule is not None:
+        sched = interpret_schedule(args.tf, *args.schedule)
+        print(sched)
+        dw_sampler.validate_anneal_schedule(sched)
+    else:
+        print(f"tf={args.tf}")
+        sched = None
+    sched_kwags = {"anneal_schedule": sched} if sched is not None else {"annealing_time": args.tf}
+    dw_kwargs = {"num_spin_reversal_transforms": 1 if args.rand_gauge else 0,
+                 "num_reads": args.num_reads,
+                 "auto_scale": False}
+    print("Constructing cell embedding...")
+    if args.cell_p < 1.0:
+        random_fill=args.cell_p
+    else:
+        random_fill=None
+    cell_sampler = PegasusCellEmbedding(16, dw_sampler, random_fill=random_fill, cache=False)
+
+    aggr_results = run_sampler(cell_sampler, bqm, args, **dw_kwargs, **sched_kwags)
+    all_results: dimod.SampleSet = dimod.concatenate(aggr_results)
+    save_cell_results(all_results, sched, args)
+
+
+if __name__ == "__main__":
+    main()
+
--- setup.py
+++ setup.py
@@ -27,5 +27,24 @@ extensions = [
 
 setup(
     name="Pegasus Tools",
+    version="0.0.1",
+    author="Humberto Munoz Bauza",
+    packages=["pegasustools"],
+    install_requires=[
+        "dimod",
+        "numpy",
+        "pandas",
+        "networkx",
+        "dwave_networkx",
+        "dwave_system"
+    ],
+    entry_points={
+        'console_scripts': [
+            'pgt-cell = pegasustools.apps.cell_anneal:main',
+            'pgt-gen = pegasustools.apps.instance_gen:main',
+            'pgt-qac-chain = pegasustools.apps.qac_chain:main',
+            'pgt-qac-top = pegasustools.apps.qac_top:main'
+        ]
+    },
     ext_modules=cythonize(extensions, annotate=True),
 )
